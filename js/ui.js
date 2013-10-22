473f44b5fb7534c4382aa05611eb8e297d57e687
Andrew <redhead2204@gmail.com>
2013-10-22 17:32:10 +0400

    finished

--- js/ui.js
+++ js/ui.js
@@ -2,11 +2,31 @@ var app = (function(){
 	return {
 		init: function() {},
 		onLoad: function() {
+			this.equalColHeight();
+			this.slider();
+		},
+		equalColHeight: function() {
 			var maxHeight = 0, columns = $('footer').find('.f-column');
 			columns.each(function() {
 				maxHeight = $(this).height() > maxHeight ? $(this).height() : maxHeight;
 			});
 			columns.height(maxHeight);
+		},
+		slider: function() {
+			var slider = $('#slider'), els = slider.find('li'), elWidth = els.eq(0).outerWidth();
+			var stopWidth = els.length * elWidth;
+			slider.html(slider.html() + slider.html());
+			var animate = function() {
+				slider.animate({
+					left: '-=' + elWidth + 'px'
+				}, 500, function() {
+					if (slider.css('left') === -stopWidth + 'px') {
+						slider.css('left', '0');
+					}
+				});
+				setTimeout(animate, 3000);
+			};
+			setTimeout(animate, 1000);
 		}
 	}
 }());
